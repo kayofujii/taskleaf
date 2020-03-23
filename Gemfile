53d2013ad278339dbaf3a9c0984b78d179d17ccb
藤井佳世 <fujiikayo@MacBook-Pro.local>
2020-03-23 21:40:39 +0900

    csvファイルをエクスポートできる

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'factory_bot_rails', '~> 4.11'
 end
 
 group :development do
@@ -48,6 +49,7 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem 'rspec-rails', git: 'https://github.com/rspec/rspec-rails.git', tag: 'v4.0.0.beta3'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
@@ -57,3 +59,4 @@ gem 'slim-rails'
 gem 'html2slim'
 gem 'bootstrap'
 gem 'rails_autolink'
+gem 'ransack'
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,14 @@
 class Task < ApplicationRecord
-  before_validation :set_nameless_name
+  # before_validation :set_nameless_name
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
 
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+  
+  has_one_attached :image
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
@@ -8,13 +16,27 @@ class Task < ApplicationRecord
   belongs_to :user
 
   scope :recent, -> { order(created_at: :desc)}
+
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end 
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes 
+      all.each do |task|
+        csv << csv_attributes.map{|attr| task.send(attr) }
+      end
+    end 
+  end 
+
   private
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
   end
 
-  def set_nameless_name
-    self.name = '名前なし' if name.blank?
-  end
+  # def set_nameless_name
+  #   self.name = '名前なし' if name.blank?
+  # end
 end
